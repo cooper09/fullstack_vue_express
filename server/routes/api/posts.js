d4bc0c41c8792c0a3ebf28f7ac344f652162ca02
cooper <cooper@BC-MG.localdomain>
2019-07-10 16:24:02 -0400

    initial commit

---
+++ server/routes/api/posts.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get Posts
+
+// Add Post
+
+
+// Delete Post
+
+module.exports = router;
